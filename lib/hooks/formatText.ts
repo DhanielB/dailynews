d199143be32ad145571658e4e2697eccd3658d8b
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-16 17:48:12 -0300

    Create formatText.ts

---
+++ lib/hooks/formatText.ts
@@ -0,0 +1,11 @@
+export default function formatText(text: string): string {
+  let output = "";
+
+  for (var i=0; i< text.length; i++) {
+      if (text.charCodeAt(i) <= 127) {
+          output += text.charAt(i);
+      }
+  }
+
+  return output.toLowerCase().replaceAll(" ", "-")
+}
