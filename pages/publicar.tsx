bc56bd1c6f220b2b62f9369363c66dd836366510
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-21 16:10:48 +0000

    Ssss

--- pages/publicar.tsx
+++ pages/publicar.tsx
@@ -6,21 +6,21 @@ export default function Publicar() {
 
   return (
     <Layout>
-      <h1 className="font-semibold md:text-[2rem] md:mt-[1.5rem] md:ml-[1.625rem]">
+      <h1 className="font-semibold text-[2rem] mt-[1.5rem] ml-[1.625rem] md:text-[2rem] md:mt-[1.5rem] md:ml-[1.625rem]">
         Publicar novo conteúdo
       </h1>
 
       <input
-        className="border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
+        className="px-4 py-[0.5rem] top-[6rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
         placeholder="Título"
       ></input>
       <div>
-        <textarea className="h-72 border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute"></textarea>
+        <textarea className="h-72 px-[8rem] py-[3rem] top-[9.725rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute"></textarea>
         <button
           onClick={() => {
             setMode("write");
           }}
-          className="write md:text-[0.9rem] md:top-[10rem] md:left-[3rem] absolute"
+          className="write text-[0.9rem] top-[10rem] left-[3rem] md:text-[0.9rem] md:top-[10rem] md:left-[3rem] absolute"
         >
           Escrever
         </button>
@@ -28,24 +28,24 @@ export default function Publicar() {
           onClick={() => {
             setMode("view");
           }}
-          className="view md:text-[0.9rem] md:top-[10rem] md:left-[7.5rem] absolute"
+          className="view text-[0.9rem] top-[10rem] left-[7.5rem] md:text-[0.9rem] md:top-[10rem] md:left-[7.5rem] absolute"
         >
           Visualizar
         </button>
       </div>
 
       <input
-        className="border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[28.75rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
+        className="px-4 py-[0.5rem] top-[28.75rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[28.75rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
         placeholder="Fonte (Opicional)"
       ></input>
 
         <button
-          className="text-gray-600 md:text-base md:top-[34rem] md:left-[52rem] absolute"
+          className="top-[33rem] left-[14rem] text-gray-600 md:text-base md:top-[34rem] md:left-[52rem] absolute"
         >
           Cancelar
         </button>
         <button
-          className="border border-gray-500 px-3 py-[0.15rem] rounded-md text-white bg-[#2DA44E] md:text-base md:top-[33.75rem] md:left-[57.5rem] absolute"
+          className="text-base top-[32.75rem] left-[19.5rem] border border-gray-500 px-3 py-[0.15rem] rounded-md text-white bg-[#2DA44E] md:text-base md:top-[33.75rem] md:left-[57.5rem] absolute"
         >
           Publicar
         </button>
