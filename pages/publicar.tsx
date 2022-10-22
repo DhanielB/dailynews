705ca9da662321a777f38e9a154c07968518fcd2
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-22 07:31:55 -0300

    Update publicar.tsx

--- pages/publicar.tsx
+++ pages/publicar.tsx
@@ -11,7 +11,7 @@ export default function Publicar() {
       </h1>
 
       <input
-        className="px-4 py-[0.5rem] top-[6rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
+        className="text-5 px-4 py-[0.5rem] top-[6rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6rem] md:left-[1.625rem] md:w-[60.75rem] absolute"
         placeholder="Título"
       ></input>
       <div>
