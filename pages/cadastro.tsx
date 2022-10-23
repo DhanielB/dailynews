84d5dfa0b58357619e49c9d747142e0904b7eb90
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-23 13:35:45 +0000

    j

--- pages/cadastro.tsx
+++ pages/cadastro.tsx
@@ -60,7 +60,7 @@ const SignUp = () => {
           Cadastro
         </h1>
 
-        <p className="text-[0.9rem] font-semibold top-[5.9rem] left-[1rem] md:left-[16rem] md:top-[5.45rem] md:text-[0.875rem] absolute">
+        <p className="text-[0.8rem] font-semibold top-[5.9rem] left-[1rem] md:left-[16rem] md:top-[5.45rem] md:text-[0.875rem] absolute">
           Nome de Usuário
         </p>
         <input
@@ -70,7 +70,7 @@ const SignUp = () => {
           className="text-5 px-4 py-[0.75rem] top-[7rem] left-[0.5rem] w-[24.7rem] border border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6.725rem] md:left-[16rem] md:w-[31.5rem] absolute"
         ></input>
 
-        <p className="text-[0.9rem] font-semibold top-[10.85rem] left-[1rem] md:left-[16rem] md:top-[11rem] md:text-[0.875rem] absolute">
+        <p className="text-[0.8rem] font-semibold top-[10.85rem] left-[1rem] md:left-[16rem] md:top-[11rem] md:text-[0.875rem] absolute">
           Email
         </p>
         <input
@@ -80,8 +80,8 @@ const SignUp = () => {
           className="text-5 px-4 py-[0.75rem] top-[12rem] left-[0.5rem] w-[24.7rem] border border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[12.5rem] md:left-[16rem] md:w-[31.5rem] absolute"
         ></input>
 
-        <button className="w-[24.7rem] text-base top-[16rem] left-[0.5rem] border border-gray-400 px-3 py-[0.35rem] rounded-md text-white bg-[#2DA44E] focus:bg-[#8ac79b] md:text-base md:top-[16rem] md:left-[16rem] md:w-[31.5rem] absolute">
-          Publicar
+        <button className="w-[24.7rem] text-base top-[16rem] left-[0.5rem] border border-gray-400 px-3 py-[0.5rem] rounded-md text-white bg-[#2DA44E] focus:bg-[#8ac79b] md:text-base md:top-[16rem] md:left-[16rem] md:w-[31.5rem] absolute">
+          Criar Cadrastro
         </button>
       </form>
     </Layout>
