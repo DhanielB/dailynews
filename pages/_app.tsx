86c77dd7b09d1ad6a928ae7a33ec72a739941804
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-23 17:28:56 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />;
-      
-      <Script src="//cdn.jsdelivr.net/npm/eruda"></Script>
-      <Script>eruda.init();</Script>
     </div>
   )
 }
