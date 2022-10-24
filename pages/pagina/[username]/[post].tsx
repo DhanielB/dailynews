6e6ccdb8dd5a2fee5e7b4b3f3567808e7ba499a8
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-24 05:53:00 -0300

    Update [post].tsx

--- pages/pagina/[username]/[post].tsx
+++ pages/pagina/[username]/[post].tsx
@@ -32,7 +32,6 @@ export default function username() {
       {newsFetched.map((news) => {
         const { title, titleSlug, by, content } = news
         
-        alert(`Post: ${post}, TitleSlug: ${titleSlug}, Username: ${username}, By: ${by}`)
         if(by == username && titleSlug == post) {
           return (
             <div className="p-[1rem]">
