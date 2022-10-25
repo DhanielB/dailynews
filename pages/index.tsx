56159b5ad847b769f04d91a74a979b1a905487e8
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-24 21:59:00 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import formatText from "../lib/hooks/formatText";
+import { formatDistance } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'; 
 
 const Home = () => {
   const router = useRouter()
@@ -22,7 +24,7 @@ const Home = () => {
   return (
     <Layout>
       <ul className="ml-[1.25rem] mt-[1.5rem] break-words md:ml-[1.25rem] md:mt-[1.5rem]">{newsFetched.map((news, newsCounted) => {
-        const { title, commentCount, by, on } = news
+        const { title, commentCount, by, createdAt } = news
 
         return (
           <li className="md:ml-4 cursor-pointer pb-2 pr-4" key={newsCounted + 1}>
@@ -31,7 +33,7 @@ const Home = () => {
           }}>{newsCounted + 1}. {title}</h1>
             <p className="text-gray-500 ml-[1rem] text-[0.8rem] md:ml-[1rem] md:text-[0.75rem]"><span>{commentCount} comentário</span> · <span className="hover:underline" onClick={() => {
             router.push(`/pagina/${formatText(by)}`)
-          }}>{by}</span> · <span>{on}</span></p>
+          }}>{by}</span> · <span>{formatDistance(Date.now(), createdAt, { locale: ptBR })}</span></p>
           </li>
         )
       })}</ul>
