28fdee88db75edf5cba48a0b2ec2705ab29a1d34
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-25 21:24:49 -0300

    Update [post].tsx

--- pages/pagina/[username]/[post].tsx
+++ pages/pagina/[username]/[post].tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import Layout from "../../../components/Layout"
-import { Viewer, Editor } from "@bytemd/react";
+import ReactMarkdown from 'react-markdown'
 import "bytemd/dist/index.min.css";
 import "bytemd/dist/index.min.css";
 import "highlight.js/styles/github.css";
@@ -35,7 +35,7 @@ export default function username() {
         if(by == username && titleSlug == post) {
           return (
             <div className="p-[1rem]">
-              <Viewer value={`# ${title} - ${by}\n${content}`}></Viewer>
+              <ReactMarkdown className="markdown-body flex flex-col overflow-auto box-border h-72 pl-[8rem] pr-[1.5rem] first-line:pr-[8rem] py-[3rem] top-[9.725rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute">{`# ${title} - ${by}\n${content}`}></ReactMarkdown>
             </div>
           )
         }
