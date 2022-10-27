c98f9281992a2d8e580c409ea87ab05669a4d54c
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-27 16:09:20 -0300

    Update publicar.tsx

--- pages/publicar.tsx
+++ pages/publicar.tsx
@@ -7,8 +7,9 @@ import Router from "next/router";
 import "highlight.js/styles/github.css";
 import "github-markdown-css/github-markdown-light.css";
 import ReactMarkdown from 'react-markdown'
-import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
 
 export default function Publicar() {
   const user = useUser({ redirectTo: "/cadastro" });
@@ -70,11 +71,8 @@ export default function Publicar() {
         <div>
           {mode == "view" ? (
             <div className="flex flex-col overflow-auto">
-              <ReactMarkdown
-                remarkPlugins={[remarkMath]}
-                rehypePlugins={[rehypeKatex]}
-                children={content}
-                className="markdown-body flex flex-col overflow-auto box-border h-72 pl-[8rem] pr-[1.5rem] first-line:pr-[8rem] py-[3rem] top-[9.725rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute"/>
+              <ReactMarkdown plugins={[remarkGfm, remarkMath, rehypeKatex]}
+                className="markdown-body flex flex-col overflow-auto box-border h-72 pl-[8rem] pr-[1.5rem] first-line:pr-[8rem] py-[3rem] top-[9.725rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute">{content}</ReactMarkdown>
             </div>
           ) : (
             <div>
