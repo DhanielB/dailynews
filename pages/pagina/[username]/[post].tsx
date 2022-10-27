96ac6242eda488984c671c23757829335e5eb81f
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-27 12:29:33 -0300

    Update [post].tsx

--- pages/pagina/[username]/[post].tsx
+++ pages/pagina/[username]/[post].tsx
@@ -1,13 +1,18 @@
+
 import axios from "axios"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import Layout from "../../../components/Layout"
-import ReactMarkdown from '../../../components/Markdown'
 import "bytemd/dist/index.min.css";
 import "bytemd/dist/index.min.css";
 import "highlight.js/styles/github.css";
 import "github-markdown-css/github-markdown-light.css";
 import findNewsHook from "../../../lib/db/findNews";
+import ReactMarkdown from 'react-markdown'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
+import 'katex/dist/katex.min.css'
 
 export default function username({ newsFetched }) {
   const router = useRouter()
@@ -21,7 +26,10 @@ export default function username({ newsFetched }) {
         if(by == username && titleSlug == post) {
           return (
             <div className="p-[1rem]">
-              <ReactMarkdown source={`# ${title} - ${by}\n${content}`} className="markdown-body"/>
+              <ReactMarkdown
+                remarkPlugins={[remarkMath]}
+                rehypePlugins={[rehypeKatex]}
+                source={`# ${title} - ${by}\n${content}`} className="markdown-body flex flex-col overflow-auto box-border h-72 pl-[8rem] pr-[1.5rem] first-line:pr-[8rem] py-[3rem] top-[9.725rem] left-[1.625rem] w-[22.5rem] border-[2px] border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-[8rem] md:py-[3rem] md:top-[9.725rem] md:left-[1.625rem] md:w-[60.75rem] absolute"/>
             </div>
           )
         }
