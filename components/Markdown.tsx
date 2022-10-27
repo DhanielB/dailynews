2632164a8a51930e1e7ac992f6a7f479c5cb39f9
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-26 23:08:51 -0300

    Update Markdown.tsx

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import MathJax from 'react-mathjax';
 import RemarkMathPlugin from 'remark-math';
-import { BlockMath, InlineMath } from 'react-katex';
-import 'katex/dist/katex.min.css';
 
-const _mapProps = (props) => ({
-  ...props,
-  escapeHtml: false,
-  plugins: [
-    RemarkMathPlugin
-  ],
-  renderers: {
-    ...props.renderers,
-    math: ({ value }) => <BlockMath>{value}</BlockMath>,
-    inlineMath: ({ value }) => <InlineMath>{value}</InlineMath>
-  }
-});
-
-const Markdown = (props) => <ReactMarkdown {..._mapProps(props)} />;
-
-export default Markdown;
+export default function MarkdownRender(props) {
+    const newProps = {
+        ...props,
+        plugins: [
+          RemarkMathPlugin,
+        ],
+        renderers: {
+          ...props.renderers,
+          math: (props) => 
+            <MathJax.Node formula={props.value} />,
+          inlineMath: (props) =>
+            <MathJax.Node inline formula={props.value} />
+        }
+      };
+      return (
+        <MathJax.Provider input="tex">
+            <ReactMarkdown {...newProps} />
+        </MathJax.Provider>
+      );
+}
