dc1125fd4214ad68abc7b1c7b231c6e3caeb80a0
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-27 19:11:54 -0300

    Update publicar.tsx

--- pages/publicar.tsx
+++ pages/publicar.tsx
@@ -10,6 +10,7 @@ import ReactMarkdown from 'react-markdown'
 import rehypeKatex from 'rehype-katex'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
+import 'katex/dist/katex.min.css'
 
 export default function Publicar() {
   const user = useUser({ redirectTo: "/cadastro" });
