79c1408c29a38d20629156d429e8d573052343ab
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-10-28 09:50:51 -0300

    Update [post].tsx

--- pages/pagina/[username]/[post].tsx
+++ pages/pagina/[username]/[post].tsx
@@ -22,7 +22,7 @@ export default function username({ newsFetched }) {
   return (
     <Layout>
       {newsFetched.map((news) => {
-        const { title, titleSlug, by, content } = news
+        const { title, titleSlug, by, content, source } = news
         
         if(by == username && titleSlug == post) {
           return (
@@ -30,7 +30,7 @@ export default function username({ newsFetched }) {
               <ReactMarkdown
                 remarkPlugins={[remarkGfm, remarkMath]}
                 rehypePlugins={[rehypeKatex]}
-                children={`# ${title} - ${by}\n${content}`}
+                children={`# ${title} - ${by}\n${content}\n\n### ${source}`}
                 className="markdown-body"/>
             </div>
           )
