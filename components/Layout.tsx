f2a7181df6e3bdc12200ab3c56ab0c3fe4a576cd
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-01 06:32:47 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Layout({ children }: { children: any }) {
   return (
-    <div className="flex w-max h-full">
+    <div className="flex w-screen h-full">
       <Header></Header>
       <div className="flex w-screen h-[45.5rem] md:h-[37rem] top-16 absolute">
         {children}
