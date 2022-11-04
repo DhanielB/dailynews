65ca54d76d2c488a091b772d9ce31ad60bd4f712
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-04 07:51:22 -0300

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -64,6 +64,7 @@ const Login = () => {
             setEmailData(e.currentTarget.value);
           }}
           type="email"
+          value={emailData}
           required
           className="text-5 px-4 py-[0.75rem] top-[7rem] left-[0.5rem] w-[24.7rem] border border-black border-opacity-20 rounded-md outline-none focus:border-[#3277ca] md:px-4 md:py-[0.5rem] md:top-[6.725rem] md:left-[16rem] md:w-[31.5rem] absolute"
         ></input>
