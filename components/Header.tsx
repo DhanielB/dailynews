90e090c9eb2e7c01a5882e31ef8093c6f7b02647
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-04 08:46:56 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUser } from "../lib/hooks/useUser";
 import axios from "axios";
 
