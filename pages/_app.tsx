08b4ce4068a39cc43c24aafea71c81d45727e473
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-06 12:15:31 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,26 @@
 import "../styles/globals.css";
 import Router from "next/router";
+import { useState, useEffect } from "react"
 
 export default function App({
   Component,
   pageProps
 }) {
-  const [loading, setLoading] = React.useState(false);
-  React.useEffect(() => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
     const start = () => {
-      console.log("start");
       setLoading(true);
     };
+
     const end = () => {
-      console.log("finished");
       setLoading(false);
     };
+
     Router.events.on("routeChangeStart", start);
     Router.events.on("routeChangeComplete", end);
     Router.events.on("routeChangeError", end);
+
     return () => {
       Router.events.off("routeChangeStart", start);
       Router.events.off("routeChangeComplete", end);
