89ab84d62657274e945c6f42f77a09df84ed4043
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-06 11:46:36 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-
+import pensador from "pensador"
 function MyApp({ Component, pageProps }) {
   const [isLoading, setIsLoading] = useState(true)
   const router = useRouter()
@@ -13,9 +13,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       {isLoading ? (
-        <p className="text-gray-400 top-[1rem] left-[8rem] md:top-[1rem] md:left-[27.5rem] absolute">
-            Carregando...
-         </p>
+        <Layout>
+          <p className="text-gray-400 top-[1rem] left-[8rem] md:top-[1rem] md:left-[27.5rem] absolute">
+            Carregando... {pensador.getFromMotivacionais().then(result => result.message)}
+          </p>
+        </Layout>
       ) : (
         <Component {...pageProps} />
       )}
