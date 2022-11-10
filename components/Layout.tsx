156c403068a29305985a5958e87e731cde1ffa93
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-11-10 10:49:04 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: any }) {
   return (
     <div className="flex w-screen h-full">
       <Header></Header>
-      <div className="flex w-screen h-[45.5rem] md:h-[37rem] top-16 absolute">
+      <div className="flex w-screen bg-[#fafafa] h-[45.5rem] md:h-[37rem] top-16 absolute">
         {children}
       </div>
       <AnalyticsWrapper />
